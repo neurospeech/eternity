51dafc194b52dde43fd608c2aefe39daf618046b
Akash Kava <39438041+ackava@users.noreply.github.com>
2022-06-21 21:30:21 +0530

    SqlStorage in progress

---
+++ NeuroSpeech.Eternity.SqlStorage/EternitySqlStorage.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Data.SqlClient;
+using NeuroSpeech.Eternity.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuroSpeech.Eternity.SqlStorage
+{
+    public class EternitySqlStorage : IEternityRepository
+    {
+        private Task? InitAsync;
+        private readonly TimeSpan QueuePollInterval;
+        private readonly string connectionString;
+        private readonly IEternityClock clock;
+
+        public EternitySqlStorage(
+            string connectionString,
+            IEternityClock clock,
+            TimeSpan queuePollInterval = default)
+        {
+            this.QueuePollInterval = queuePollInterval.Ticks == 0
+                ? TimeSpan.FromSeconds(15)
+                : queuePollInterval;
+            this.connectionString = connectionString;
+            this.clock = clock;
+        }
+
+        public async Task<SqlConnection> Open()
+        {
+            var conn = new SqlConnection(connectionString);
+            if (InitAsync == null)
+            {
+                InitAsync = ModelCreator.CreateAsync(conn);
+            }
+            await InitAsync;
+            return conn;
+        }
+
+        public Task<string> CreateAsync(EternityEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(EternityEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteChildrenAsync(EternityEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<EternityEntity> GetAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IAsyncDisposable> LockAsync(EternityEntity entity, TimeSpan maxTTL)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<EternityEntity>> QueryAsync(int max, DateTimeOffset utcNow)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync(params EternityEntity[] entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NeuroSpeech.Eternity.SqlStorage/ModelGenerator.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Data.SqlClient;
+using NeuroSpeech.TemplatedQuery;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuroSpeech.Eternity
+{
+    internal class ModelCreator
+    {
+        internal static async Task CreateAsync(SqlConnection conn)
+        {
+            // create workflows table..
+            var createScript = TemplateQuery.New(@$"
+    IF object_id('MyTable') is null
+    CREATE TABLE EternityEntities (
+        ID TEXT PRIMARY KEY,
+        Name TEXT NOT NULL,
+        Input TEXT,
+        IsWorkflow BOOLEAN,
+        UtcETA INTEGER NOT NULL,
+        UtcCreated INTEGER NOT NULL,
+        UtcUpdated INTEGER NOT NULL,
+        Response TEXT,
+        State TEXT,
+        ParentID TEXT,
+        Priority INTEGER,
+        CurrentWaitingID TEXT
+    );
+
+    CREATE INDEX IF NOT EXISTS IX_Workflows_ParentID
+    ON EternityEntities (ParentID) WHERE ParentID IS NOT NULL;
+
+    CREATE INDEX IF NOT EXISTS IX_Workflows_UtcETA
+    ON EternityEntities (UtcETA) WHERE IsWorkflow = 1;
+
+    CREATE TABLE IF NOT EXISTS ActivityLocks (
+        ID TEXT PRIMARY KEY,
+        ETA INTEGER
+    );
+
+    DELETE FROM ActivityLocks;
+
+");
+            await conn.ExecuteNonQueryAsync(createScript);
+
+            await conn.ExecuteNonQueryAsync(TemplateQuery.New($"PRAGMA journal_mode = 'wal'"));
+        }
+    }
+
+    public class ActivityLock
+    {
+        public string ID { get; set; }
+        public DateTime ETA { get; set; }
+
+    }
+}
